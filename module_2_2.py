50a36d43daa8ab5c046a683bb7451fa0edc79c78
Yofurr <distrvlad@gmail.com>
2024-12-02 14:03:42 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+# Задача "Все ли равны?"
+
+first = input('Введите целое число: ')
+second = input('Введите целое число: ')
+third = input('Введите целое число: ')
+
+if first == second == third:
+    print(3)
+elif first != second != third:
+    print(0)
+else:
+    print(2)
